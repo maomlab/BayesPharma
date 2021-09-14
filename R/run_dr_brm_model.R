c86cffa6b767c48483307b8b7ce5b703476099f9
MadelineMartin <martin.mjm105@gmail.com>
2021-09-14 16:01:34 -0400

    added rename columns

--- R/run_dr_brm_model.R
+++ R/run_dr_brm_model.R
@@ -21,22 +21,24 @@ single_drug_model <- function(data,
                               iter = 8000,
                               warmup = 4000,
                               chains = 4,
+                              sample_prior = "no",
                               adapt_delta = 0.99,
                               max_treedepth = 10){
 
-  dplyr::rename(data, response = response_col_name)
-
-  dplyr::rename(data, log_dose = log_dose_col_name)
+  input_data <- data %>%
+    dplyr::rename(response = response_col_name) %>%
+    dplyr::rename(log_dose = log_dose_col_name)
 
   brms::brm(
     formula = brms::brmsformula(
       response ~ (bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill))),
       ec50 + hill + top + bottom ~ 1, nl = TRUE),
-    data = data,
+    data = input_data,
     prior = priors,
     inits = inits,
     iter = iter,
     warmup = warmup,
+    sample_prior = sample_prior,
     control = list(adapt_delta = adapt_delta, max_treedepth = max_treedepth)
   )
   }
@@ -65,34 +67,25 @@ multi_drug_model <- function(data,
                              iter = 8000,
                              warmup = 4000,
                              chains = 4,
+                             sample_prior = "no",
                              adapt_delta = 0.99,
                              max_treedepth = 10){
 
-  if (response_col_name == response){
-    NULL
-  } else{
-    dplyr::rename(data, response = response_col_name)
-  }
-  if (log_dose_col_name == log_dose){
-    NULL
-  } else{
-    dplyr::rename(data, log_dose = log_dose_col_name)
-  }
-  if (drug_col_name == drug){
-    NULL
-  } else{
-    dplyr::rename(data, drug = drug_col_name)
-  }
+  input_data <- data %>%
+    dplyr::rename(response = response_col_name) %>%
+    dplyr::rename(log_dose = log_dose_col_name) %>%
+    dplyr::rename(drug = drug_col_name)
 
   brms::brm(
     formula = brms::brmsformula(
       response ~ (bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill))),
       ec50 + hill + top + bottom ~ 0 + drug, nl = TRUE),
-    data = data,
+    data = input_data,
     prior = priors,
     inits = inits,
     iter = iter,
     warmup = warmup,
+    sample_prior = sample_prior,
     control = list(adapt_delta = adapt_delta, max_treedepth = max_treedepth)
   )
 }
