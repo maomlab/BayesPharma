2ac590f1cb4361c9f0c1f9883f2e2da2e38f1e88
MadelineMartin <martin.mjm105@gmail.com>
2021-09-14 15:59:27 -0400

    made edits

--- R/data_preparation.R
+++ R/data_preparation.R
@@ -1,13 +1,15 @@
+#' Adds a log dose column to the tibble or data.frame of the data
 #'
+#'@param data tibble or data.frame
+#'@param dose_col string object
+#'@param molar_concentration numeric object
+#'@return tibble or data.frame
 #'
-#'
-#'
-#'
-#'
-#'
+#'@export
 
 calc_log_dose <- function(data,
                           dose_col,
                           molar_concentration){
   data %>% dplyr::mutate(log_dose = dose_col * molar_concentration)
 }
+
