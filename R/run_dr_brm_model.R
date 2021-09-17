5a8952406f279f1f9e767425381f70898f477afa
MadelineMartin <martin.mjm105@gmail.com>
2021-09-17 16:50:54 -0400

    edits to improve fxns

--- R/run_dr_brm_model.R
+++ R/run_dr_brm_model.R
@@ -1,3 +1,21 @@
+#'
+#'
+#'
+#'
+#'
+
+dose_response_formula <- function(predictors = 1, ...){
+
+  predictor_formula = rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
+                                         rhs = rlang::enexpr(predictors))
+
+  sigmoid_formula = brms::brmsformula(
+    response ~ (bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill))),
+    predictor_formula, nl = TRUE, ...)
+
+  return(sigmoid_formula)
+}
+
 #' title
 #'
 #'@param data
@@ -13,80 +31,31 @@
 #'
 #'@export
 
-single_drug_model <- function(data,
-                              response_col_name,
-                              log_dose_col_name,
-                              priors = NULL,
-                              inits = 0,
-                              iter = 8000,
-                              warmup = 4000,
-                              chains = 4,
-                              sample_prior = "no",
-                              adapt_delta = 0.99,
-                              max_treedepth = 10){
+dose_response_model <- function(data,
+                                response_col_name,
+                                log_dose_col_name,
+                                sigmoid_formula = dose_response_formula(),
+                                priors = NULL,
+                                inits = 0,
+                                iter = 8000,
+                                control = list(adapt_delta = 0.99),
+                                ...){
 
+  if (is.null(priors)){
+    stop("priors for ec50, hill, top and bottom are required. Use make_priors function to get default priors.")
+  }
+
+  # this will probably be removed later
+  # will say the input tibble/data.frame needs to contain response and log_dose
   input_data <- data %>%
     dplyr::rename(response = response_col_name) %>%
     dplyr::rename(log_dose = log_dose_col_name)
 
   brms::brm(
-    formula = brms::brmsformula(
-      response ~ (bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill))),
-      ec50 + hill + top + bottom ~ 1, nl = TRUE),
+    formula = sigmoid_formula,
     data = input_data,
     prior = priors,
     inits = inits,
-    iter = iter,
-    warmup = warmup,
-    sample_prior = sample_prior,
-    control = list(adapt_delta = adapt_delta, max_treedepth = max_treedepth)
-  )
+    control = control,
+    ...)
   }
-
-#' title
-#'
-#'@param data
-#'@param priors
-#'@param inits
-#'@param iter
-#'@param warmup
-#'@param chains
-#'@param adapt_delta
-#'@param max_treedepth
-#'
-#'@return brmsfit
-#'
-#'@export
-
-multi_drug_model <- function(data,
-                             response_col_name,
-                             log_dose_col_name,
-                             drug_col_name,
-                             priors = NULL,
-                             inits = 0,
-                             iter = 8000,
-                             warmup = 4000,
-                             chains = 4,
-                             sample_prior = "no",
-                             adapt_delta = 0.99,
-                             max_treedepth = 10){
-
-  input_data <- data %>%
-    dplyr::rename(response = response_col_name) %>%
-    dplyr::rename(log_dose = log_dose_col_name) %>%
-    dplyr::rename(drug = drug_col_name)
-
-  brms::brm(
-    formula = brms::brmsformula(
-      response ~ (bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill))),
-      ec50 + hill + top + bottom ~ 0 + drug, nl = TRUE),
-    data = input_data,
-    prior = priors,
-    inits = inits,
-    iter = iter,
-    warmup = warmup,
-    sample_prior = sample_prior,
-    control = list(adapt_delta = adapt_delta, max_treedepth = max_treedepth)
-  )
-}
-
