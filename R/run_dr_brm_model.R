b183e1149c5924466f6b4b58ac873315df221698
MadelineMartin <martin.mjm105@gmail.com>
2021-09-27 16:43:57 -0400

    created different dr formula fxns for predictors & no predictor

--- R/run_dr_brm_model.R
+++ R/run_dr_brm_model.R
@@ -4,7 +4,26 @@
 #'
 #'
 
-dose_response_formula <- function(predictors = 1, ...){
+no_predictors_dr_formula <- function(...){
+
+  predictor_formula = rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
+                                         rhs = quote(1))
+
+  sigmoid_formula = brms::brmsformula(
+    response ~ (bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill))),
+    predictor_formula, nl = TRUE, ...)
+
+  return(sigmoid_formula)
+
+}
+
+#'
+#'
+#'
+#'
+#'
+
+predictors_dr_formula <- function(predictors = 0 + predictors, ...){
 
   predictor_formula = rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
                                          rhs = rlang::enexpr(predictors))
@@ -14,7 +33,9 @@ dose_response_formula <- function(predictors = 1, ...){
     predictor_formula, nl = TRUE, ...)
 
   return(sigmoid_formula)
+
 }
+
 
 #' title
 #'
@@ -34,7 +55,8 @@ dose_response_formula <- function(predictors = 1, ...){
 dose_response_model <- function(data,
                                 response_col_name,
                                 log_dose_col_name,
-                                sigmoid_formula = dose_response_formula(),
+                                predictors_col_name = NULL,
+                                sigmoid_formula = no_predictors_dr_formula(),
                                 priors = NULL,
                                 inits = 0,
                                 iter = 8000,
@@ -49,13 +71,15 @@ dose_response_model <- function(data,
   # will say the input tibble/data.frame needs to contain response and log_dose
   input_data <- data %>%
     dplyr::rename(response = response_col_name) %>%
-    dplyr::rename(log_dose = log_dose_col_name)
+    dplyr::rename(log_dose = log_dose_col_name) %>%
+    dplyr::rename(predictors = predictors_col_name)
 
   brms::brm(
     formula = sigmoid_formula,
     data = input_data,
     prior = priors,
     inits = inits,
+    iter = iter,
     control = control,
     ...)
   }
