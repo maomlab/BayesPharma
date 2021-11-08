c90d04afdc6cbd4a150249ac7d362380f10d8d3e
MadelineMartin <martin.mjm105@gmail.com>
2021-11-08 14:32:05 -0500

    fixed errors

--- R/run_dr_brm_model.R
+++ R/run_dr_brm_model.R
@@ -1,92 +1,4 @@
-#'Create a formula for a singular perturbation/experimental variable for the
-#'brms model
-#'
-#'additional arguments for brmsformula can be added.
-#'Reference this website for more information on additional arguments:
-#'https://paul-buerkner.github.io/brms/reference/brmsformula.html
-#'
-#'@return brmsformula
-#'
-#'@export
 
-no_predictors_dr_formula <- function(...) {
-
-  predictor_formula <- rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
-                                         rhs = quote(1))
-
-  sigmoid_formula <- brms::brmsformula(
-    response ~ (bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill))),
-    predictor_formula, nl = TRUE, ...)
-
-  return(sigmoid_formula)
-
-}
-
-#'Create a formula for a multiple different perturbations/experimental variables
-#'for the brms model
-#'
-#'@param predictors expression of the grouping variables for the parameters.
-#'i.e. what perturbations/experimental differences should be modeled separately?
-#'
-#'
-
-predictors_dr_formula <- function(predictors = 0 + predictors, ...) {
-
-  predictor_formula <- rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
-                                         rhs = rlang::enexpr(predictors))
-
-  sigmoid_formula <- brms::brmsformula(
-    response ~ (bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill))),
-    predictor_formula, nl = TRUE, ...)
-
-  return(sigmoid_formula)
-
-}
-
-
-#'Run Bayesian Regression Model using Stan
-#'
-#'For additional information on additional function arguments, reference:
-#'https://paul-buerkner.github.io/brms/reference/brm.html
-#'or
-#'https://rdrr.io/cran/rstan/man/stan.html
-#'
-#'@param data tibble or data.frame of experimental data.
-#'@param priors brmspriors data.frame for ec50, hill, top, and bottom.
-#'Use 'dr_priors' to create priors to use here.
-#'@param inits list of lists, numeric value, or "random" for the initial values
-#'of the parameters being modeled. (default = 0)
-#'@param iter number of iterations the model runs. Increasing iter can help with
-#'model convergence. (default = 8000)
-#'@param control a named list of parameters to control the sampler's behavior.
-#'Adding max_treedepth and giving a greater value than 10 can improve model
-#'convergence. (default = list(adapt_delta = 0.99))
-#'
-#'@return brmsfit
-#'
-#'@export
-
-dose_response_model <- function(data,
-                                sigmoid_formula = no_predictors_dr_formula(),
-                                priors = NULL,
-                                inits = 0,
-                                iter = 8000,
-                                control = list(adapt_delta = 0.99),
-                                ...) {
-
-  if (is.null(priors)) {
-    stop("priors for ec50, hill, top and bottom are required. Use make_priors function to get default priors.")
-  }
-
-  brms::brm(
-    formula = sigmoid_formula,
-    data = input_data,
-    prior = priors,
-    inits = inits,
-    iter = iter,
-    control = control,
-    ...)
-  }
 
 #' stanvar function
 
@@ -98,9 +10,52 @@ dr_stanvar <- brms::stanvar(
     "      real top,",
     "      real bottom,",
     "      real log_dose) {",
-    "         return (bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill)));",
+    "        if( log_dose > negative_infinity() ){",
+    "          return (bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill)));",
+    "        } else { ",
+    "           if( hill > 0) {",
+    "            return bottom;",
+    "            } else {",
+    "               return top;",
+    "            }",
+    "        }",
     "   }", sep = "\n"),
   block = "functions")
+
+#'Create a formula for a multiple different perturbations/experimental variables
+#'for the brms model
+#'
+#'@param multiple_perturbations TRUE/FALSE. If FALSE, the model will produce a
+#'singular estimate for each parameter. If TRUE, the parameter will produce
+#'parameter estimates for each perturbation.
+#'@param predictors Additional formula objects to specify predictors of non-linear
+#'parameters. i.e. what perturbations/experimental differences should be modeled
+#'separately? (Default: 0 + predictors) should a random effect be taken into consideration?
+#'i.e. cell number, plate number, etc.
+#'@return brmsformula
+#'
+#'@export
+
+dr_formula <- function(multiple_perturbations = FALSE,
+                       predictors = 0 + predictors,
+                       ...){
+
+  if (multiple_perturbations == FALSE) {
+    predictor_eq <- rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
+                                       rhs = quote(1))
+  } else{
+    predictor_eq <- rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
+                                       rhs = rlang::enexpr(predictors))
+  }
+
+  sigmoid_formula <- brms::brmsformula(
+    response ~ sigmoid(ec50, hill, top, bottom, log_dose),
+    predictor_eq, nl = TRUE, ...)
+
+  return(sigmoid_formula)
+}
+
+
 
 #'Run Bayesian Regression Model using Stan
 #'
@@ -127,32 +82,20 @@ dr_stanvar <- brms::stanvar(
 #'
 #'@export
 
-dr_stanvar_model <- function(data,
-                             multiple_perturbations = FALSE,
-                             priors = NULL,
-                             inits = 0,
-                             iter = 8000,
-                             control = list(adapt_delta = 0.99),
-                             predictors = 0 + predictors,
-                             ...) {
+dr_model <- function(data,
+                     formula = dr_formula(),
+                     priors = NULL,
+                     inits = 0,
+                     iter = 8000,
+                     control = list(adapt_delta = 0.99),
+                     ...) {
 
   if (is.null(priors)) {
     stop("priors for ec50, hill, top and bottom are required. Use make_priors function to get default priors.")
   }
 
-  if (multiple_perturbations == FALSE) {
-    predictor_eq <- rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
-                                       rhs = quote(1))
-  } else{
-    predictor_eq <- rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
-                                       rhs = rlang::enexpr(predictors))
-  }
-  data[data == -Inf] <- -100
-
   brms::brm(
-    formula = brms::brmsformula(
-      response ~ sigmoid(ec50, hill, top, bottom, log_dose),
-      predictor_eq, nl = TRUE, ...),
+    formula = formula,
     data = data,
     prior = priors,
     inits = inits,
