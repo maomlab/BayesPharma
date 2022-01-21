41d99053672934c4b4313e48d256804cc77b4086
Matthew O'Meara <maom@umich.edu>
2022-01-21 14:37:07 -0500

    Fix a bunch of issues with test-dr_formula

--- tests/testthat/test-dr_formula.R
+++ tests/testthat/test-dr_formula.R
@@ -5,7 +5,7 @@ testthat::test_that("dr_formula with a constant predictor", {
   formula <- BayesPharma::dr_formula()
 
   testthat::expect_true("brmsformula" %in% class(formula))
-  testthat::expect_equal(formula$resp = "response")
+  testthat::expect_equal(formula$resp, "response")
   testthat::expect_equal(
     rlang::f_lhs(formula$formula),
     quote(response))
@@ -13,21 +13,23 @@ testthat::test_that("dr_formula with a constant predictor", {
     rlang::f_rhs(formula$formula),
     quote(sigmoid(ec50, hill, top, bottom, log_dose)))
 
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[1]), quote(ec50))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[1]), 1L)
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[2]), quote(hill))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[2]), 1L)
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[3]), quote(top))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[3]), 1L)
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[3]), quote(bottom))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[3]), 1L)
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[1]]), quote(ec50))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[1]]), 1L)
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[2]]), quote(hill))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[2]]), 1L)
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[3]]), quote(top))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[3]]), 1L)
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[4]]), quote(bottom))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[4]]), 1L)
 })
 
 testthat::test_that("dr_formula with a substance predictor", {
-  formula <- BayesPharma::dr_formula(predictors = substance)
+  formula <- BayesPharma::dr_formula(
+    multiple_perturbations = TRUE,
+    predictors = substance)
 
   testthat::expect_true("brmsformula" %in% class(formula))
-  testthat::expect_equal(formula$resp = "response")
+  testthat::expect_equal(formula$resp, "response")
   testthat::expect_equal(
     rlang::f_lhs(formula$formula),
     quote(response))
@@ -35,21 +37,23 @@ testthat::test_that("dr_formula with a substance predictor", {
     rlang::f_rhs(formula$formula),
     quote(sigmoid(ec50, hill, top, bottom, log_dose)))
 
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[1]), quote(ec50))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[1]), quote(substance))
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[2]), quote(hill))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[2]), quote(substance))
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[3]), quote(top))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[3]), quote(substance))
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[3]), quote(bottom))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[3]), quote(substance))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[1]]), quote(ec50))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[1]]), quote(substance))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[2]]), quote(hill))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[2]]), quote(substance))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[3]]), quote(top))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[3]]), quote(substance))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[4]]), quote(bottom))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[4]]), quote(substance))
 })
 
 testthat::test_that("dr_formula with a grouped predictor", {
-  formula <- BayesPharma::dr_formula(predictors = substance + (1 | batch))
+  formula <- BayesPharma::dr_formula(
+    multiple_perturbations = TRUE,
+    predictors = substance + (1 | batch))
 
   testthat::expect_true("brmsformula" %in% class(formula))
-  testthat::expect_equal(formula$resp = "response")
+  testthat::expect_equal(formula$resp, "response")
   testthat::expect_equal(
     rlang::f_lhs(formula$formula),
     quote(response))
@@ -57,13 +61,13 @@ testthat::test_that("dr_formula with a grouped predictor", {
     rlang::f_rhs(formula$formula),
     quote(sigmoid(ec50, hill, top, bottom, log_dose)))
 
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[1]), quote(ec50))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[1]), quote(substance + (1 | batch)))
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[2]), quote(hill))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[2]), quote(substance + (1 | batch)))
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[3]), quote(top))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[3]), quote(substance + (1 | batch)))
-  testthat::expect_equal(rlang::f_lhs(formula$pforms[3]), quote(bottom))
-  testthat::expect_equal(rlang::f_rhs(formula$fforms[3]), quote(substance + (1 | batch)))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[1]]), quote(ec50))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[1]]), quote(substance + (1 | batch)))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[2]]), quote(hill))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[2]]), quote(substance + (1 | batch)))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[3]]), quote(top))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[3]]), quote(substance + (1 | batch)))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[4]]), quote(bottom))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[4]]), quote(substance + (1 | batch)))
 })
 
