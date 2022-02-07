8015cc9f402ad8ba6b9edf75a451753e2425a3de
MadelineMartin <martin.mjm105@gmail.com>
2022-02-07 14:17:49 -0500

    improved documentation

--- R/run_dr_brm_model.R
+++ R/run_dr_brm_model.R
@@ -1,6 +1,149 @@
 
+#' Create a sigmoid formula for the brms model
+#'
+#' @description set-up a sigmoid dose response model formula to define a non-linear
+#'   model or multilevel non-linear model for ec50, hill, top, and bottom for use in bayesian_model
+#'   and in the brms package.
+#'
+#' @usage
+#'    dr_formula(predictors = 1)
+#'
+#' @param predictors Additional formula objects to specify predictors of non-linear
+#'   parameters. i.e. what perturbations/experimental differences should be modeled
+#'   separately? (Default: 1) should a random effect be taken into consideration?
+#'   i.e. cell number, plate number, etc.
+#' @return brmsformula
+#'
+#' @examples
+#'   Consider observations made using 4 different drugs and the column header containing the labels
+#'   for the 4 different drugs is `predictors`.
+#'   dr_formula(predictors = 0 + predictors)
+#'
+#'   Consider that the cell_ID was recorded and the noise from the cell_ID is to be accounted for.
+#'   dr_formula(predictors = 0 + predictors + (1|cell_ID))
+#'
+#'
+#'@export
 
-#' stanvar function
+dr_formula <- function(predictors = 1,
+                       ...){
+
+
+    predictor_eq <- rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
+                                       rhs = rlang::enexpr(predictors))
+
+  sigmoid_formula <- brms::brmsformula(
+    response ~ sigmoid(ec50, hill, top, bottom, log_dose),
+    predictor_eq, nl = TRUE, ...)
+
+  return(sigmoid_formula)
+}
+
+#' Formula for a curve with a constant response
+#'
+#' @description set-up an equation for formula objects with a constant response.
+#'
+#' @usage
+#'   constant_formula(predictors = 1)
+#'
+#' @param predictors Additional formula objects to specify predictors of non-linear
+#'   parameters. i.e. what perturbations/experimental differences should be modeled
+#'   separately? (Default: 1) should a random effect be taken into consideration?
+#'   i.e. cell number, plate number, etc.
+#' @return brmsformula
+#'
+#' @examples
+#'   constant_formula(predictors = 0 + predictors)
+#'
+#' @export
+
+
+constant_formula <- function(predictors = 1,
+                       ...){
+
+    constant_eq <- rlang::new_formula(lhs = quote(response),
+                                      rhs = rlang::enexpr(predictors))
+
+  formula <- brms::brmsformula(constant_eq, ...)
+
+  return(formula)
+}
+
+
+#' Run Bayesian Regression Model using Stan
+#'
+#' @description
+#' For additional information on additional function arguments, reference:
+#' https://paul-buerkner.github.io/brms/reference/brm.html
+#' or
+#' https://rdrr.io/cran/rstan/man/stan.html
+#'
+#' @usage
+#'   dr_model(data = <data>,
+#'            formula = dr_formula(),
+#'            priors = dr_prior(),
+#'            inits = 0,
+#'            iter = 8000,
+#'            control = list(adapt_delta = 0.99),
+#'            stanvar_function = dr_stanvar)
+#'
+#' @param data data.frame of experimental data.
+#' @param model_name variable name for the model.
+#' @param priors brmspriors data.frame for ec50, hill, top, and bottom.
+#'   Use one of the priors functions provided to create priors to use here.
+#' @param inits list of lists, numeric value, or "random" for the initial values
+#'   of the parameters being modeled (default = 0).
+#' @param iter number of iterations the model runs. Increasing iter can help with
+#'   model convergence (default = 8000).
+#' @param control a named list of parameters to control the sampler's behavior.
+#'   Adding max_treedepth and giving a greater value than 10 can improve model
+#'   convergence (default = list(adapt_delta = 0.99)).
+#' @return brmsfit model
+#'
+#' @examples
+#'   dr_model(data,
+#'    formula = dr_formula(predictors = 1 + predictors),
+#'    priors = dr_priors(),
+#'    inits = dr_inits(),
+#'    iter = 8000,
+#'    control = list(adapt_delta = 0.99),
+#'    stanvar_function = dr_stanvar)
+#'
+#' @export
+
+dr_model <- function(data,
+                     model_name,
+                     formula,
+                     priors = NULL,
+                     inits = 0,
+                     iter = 8000,
+                     control = list(adapt_delta = 0.99),
+                     stanvar_function = dr_stanvar,
+                     ...) {
+
+  if (is.null(priors)) {
+    warning("priors for each parameter is required. Use prior functions provided to get default priors.")
+  }
+  model_name <- brms::brm(
+    formula = formula,
+    data = data,
+    prior = priors,
+    inits = inits,
+    iter = iter,
+    control = control,
+    stanvars = stanvar_function,
+    ...)
+
+  return(model_name)
+}
+
+#' stanvar script of a sigmoid dose response equation.
+#' For log dose greater than negative infinity (dose greater than 0), the sigmoid
+#' dose response equation is used. ec50 is the half maximal response, hill is the slope,
+#' top is the maximum response, and bottom is the minimum response.
+#' For log dose equal to negative infinity (dose equal to 0), if hill is positive,
+#' then the bottom value is returned. If hill is negative, then the top value is returned.
+#'
 dr_stanvar <- brms::stanvar(
   scode = paste(
     "   real sigmoid(",
@@ -20,114 +163,3 @@ dr_stanvar <- brms::stanvar(
     "        }",
     "   }", sep = "\n"),
   block = "functions")
-
-#'Create a formula for a one or multiple different perturbations/experimental
-#'variables for the brms model
-#'
-#'@param multiple_perturbations TRUE/FALSE. If FALSE, the model will produce a
-#'singular estimate for each parameter. If TRUE, the parameter will produce
-#'parameter estimates for each perturbation.
-#'@param predictors Additional formula objects to specify predictors of non-linear
-#'parameters. i.e. what perturbations/experimental differences should be modeled
-#'separately? (Default: 0 + predictors) should a random effect be taken into consideration?
-#'i.e. cell number, plate number, etc.
-#'@return brmsformula
-#'
-#'@export
-
-dr_formula <- function(multiple_perturbations = FALSE,
-                       predictors = 0 + predictors,
-                       ...){
-
-  if (multiple_perturbations == FALSE) {
-    predictor_eq <- rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
-                                       rhs = quote(1))
-  } else{
-    predictor_eq <- rlang::new_formula(lhs = quote(ec50 + hill + top + bottom),
-                                       rhs = rlang::enexpr(predictors))
-  }
-
-  sigmoid_formula <- brms::brmsformula(
-    response ~ sigmoid(ec50, hill, top, bottom, log_dose),
-    predictor_eq, nl = TRUE, ...)
-
-  return(sigmoid_formula)
-}
-
-#' Formula for the constant curve
-#'
-#'@param multiple_perturbations TRUE/FALSE. If FALSE, the model will produce a
-#'singular estimate for each parameter. If TRUE, the parameter will produce
-#'parameter estimates for each perturbation.
-#'@param predictors Additional formula objects to specify predictors of non-linear
-#'parameters. i.e. what perturbations/experimental differences should be modeled
-#'separately? (Default: 0 + predictors) should a random effect be taken into consideration?
-#'i.e. cell number, plate number, etc.
-#'@return brmsformula
-#'
-#'@export
-constant_formula <- function(multiple_perturbations = FALSE,
-                       predictors = 0 + predictors,
-                       ...){
-
-  if (multiple_perturbations == FALSE) {
-    constant_eq <- rlang::new_formula(lhs = quote(response),
-                                      rhs = quote(1))
-  } else{
-    constant_eq <- rlang::new_formula(lhs = quote(response),
-                                      rhs = rlang::enexpr(predictors))
-  }
-
-
-  formula <- brms::brmsformula(constant_eq, ...)
-
-  return(formula)
-}
-
-
-
-#'Run Bayesian Regression Model using Stan
-#'
-#'For additional information on additional function arguments, reference:
-#'https://paul-buerkner.github.io/brms/reference/brm.html
-#'or
-#'https://rdrr.io/cran/rstan/man/stan.html
-#'
-#'@param data tibble or data.frame of experimental data.
-#'@param multiple_perturbations TRUE/FALSE. If FALSE, the model will produce a
-#'singular estimate for each parameter. If TRUE, the parameter will produce
-#'parameter estimates for each perturbation.
-#'@param priors brmspriors data.frame for ec50, hill, top, and bottom.
-#'Use 'dr_priors' to create priors to use here.
-#'@param inits list of lists, numeric value, or "random" for the initial values
-#'of the parameters being modeled. (default = 0)
-#'@param iter number of iterations the model runs. Increasing iter can help with
-#'model convergence. (default = 8000)
-#'@param control a named list of parameters to control the sampler's behavior.
-#'Adding max_treedepth and giving a greater value than 10 can improve model
-#'convergence. (default = list(adapt_delta = 0.99))
-#'
-#'@return brmsfit
-#'
-#'@export
-dr_model <- function(data,
-                     formula = dr_formula(),
-                     priors = NULL,
-                     inits = 0,
-                     iter = 8000,
-                     control = list(adapt_delta = 0.99),
-                     ...) {
-
-  if (is.null(priors)) {
-    warning("priors for ec50, hill, top and bottom are required. Use dr_priors function to get default priors.")
-  }
-  brms::brm(
-    formula = formula,
-    data = data,
-    prior = priors,
-    inits = inits,
-    iter = iter,
-    control = control,
-    stanvars = dr_stanvar,
-    ...)
-}
