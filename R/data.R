fa4d9b135ec990b82b02163f3709e43d5d89bb32
MadelineMartin <martin.mjm105@gmail.com>
2022-03-11 10:40:01 -0500

    added r data for kor vignette

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Dose Response Measurements of Four Different Kappa Opioid Receptor
+#' Antagonists
+#'
+#' A dataset containing the drugs used, cell id, the log dose that the response
+#' was measured at, and the reference, raw, and normalized measurement
+#'
+#' @format A data.frame that includes the dose-response measurements
+#' @source \url{https://github.com/maomlab/BayesPharma}
+"kor_antag"
