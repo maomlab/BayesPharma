20e6ad306669adf501211e0dcb657f2ff46ff460
MadelineMartin <martin.mjm105@gmail.com>
2022-03-18 12:32:20 -0400

    added sigmoid fxn

--- R/sigmoid_formula.R
+++ R/sigmoid_formula.R
@@ -1,5 +1,15 @@
 
-#' Sigmoid function
+#' Sigmoid Formula
+#'
+#' @description A vectorized function of the sigmoid dose-response curve
+#'   equation. This may be need if stan backend is cmdstanr
+#'
+#' @param ec50 numeric.
+#' @param hill numeric.
+#' @param top numeric.
+#' @param bottom numeric.
+#' @param log_dose numeric.
+#' @return numeric.
 #'
 #' @export
 
