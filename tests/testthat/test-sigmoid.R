4dc0b5a453e3458c46a1833b37d54f5752487097
MadelineMartin <martin.mjm105@gmail.com>
2022-03-18 12:32:20 -0400

    added sigmoid fxn

---
+++ tests/testthat/test-sigmoid.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+testthat::test_that("sigmoid returns numeric class:", {
+  testthat::expect_equal(class(BayesPharma::sigmoid(-9, -1, 100, 0, -10)),
+                         "numeric")
+})
+
+testthat::test_that("sigmoid returns correct numeric value:", {
+  testthat::expect_equal(BayesPharma::sigmoid(-9, -1, 100, 0, -10), 90.90909)
+})
