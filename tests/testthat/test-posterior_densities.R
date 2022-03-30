8a1da09a193194a96ca11d6cc0cb07882478ced4
MadelineMartin <martin.mjm105@gmail.com>
2022-03-30 16:43:40 -0400

    created ggplot tests

---
+++ tests/testthat/test-posterior_densities.R
@@ -0,0 +1,42 @@
+library(tidymodels)
+library(BayesPharma)
+
+load(file = "../testdata/ggplot_test_model.rda")
+
+testthat::test_that("traceplot is a ggplot object",{
+  expect_gg(BayesPharma::posterior_densities(test_model))
+  expect_gg(BayesPharma::posterior_densities(test_model,
+    predictors_col_name = "_Intercept",
+    half_max_label = "ic50",
+    l_ci = 0.025,
+    u_ci = 0.975,
+    title_label = "Posterior Density Plots with Mean and 95% CI"))
+  expect_gg(BayesPharma::posterior_densities(test_model,
+    predictors_col_name = "_Intercept",
+    half_max_label = "ic50",
+    l_ci = 0.05,
+    u_ci = 0.95,
+    title_label = "Posterior Density Plots with Mean and 90% CI"))
+  expect_gg(BayesPharma::posterior_densities(test_model,
+    predictors_col_name = "_Intercept",
+    half_max_label = "ic50",
+    l_ci = 0.025,
+    u_ci = 0.975,
+    title_label = NULL))
+
+})
+
+
+testthat::test_that("traceplot throws error if predictors_col_name and
+                    half_max_label are not characters",{
+  expect_error(expect_gg(BayesPharma::posterior_densities(test_model,
+    predictors_col_name = NULL)))
+  expect_error(expect_gg(BayesPharma::posterior_densities(test_model,
+    predictors_col_name = NULL,
+    half_max_label = NULL)))
+  expect_error(expect_gg(BayesPharma::posterior_densities(test_model,
+    half_max_label = NULL)))
+
+})
+
+
---
+++ tests/testthat/test-traceplot.R
@@ -0,0 +1,32 @@
+library(tidymodels)
+library(BayesPharma)
+
+load(file = "../testdata/ggplot_test_model.rda")
+
+testthat::test_that("traceplot is a ggplot object",{
+  expect_gg(BayesPharma::traceplot(test_model))
+  expect_gg(BayesPharma::traceplot(test_model,
+    predictors_col_name = "_Intercept",
+    half_max_label = "ic50",
+    title = "test_model traceplot"))
+  expect_gg(BayesPharma::traceplot(test_model,
+    predictors_col_name = "_Intercept",
+    half_max_label = "ic50",
+    title = NULL))
+
+})
+
+
+testthat::test_that("traceplot throws error if predictors_col_name and
+                    half_max_label are not characters",{
+  expect_error(expect_gg(BayesPharma::traceplot(test_model,
+    predictors_col_name = NULL)))
+  expect_error(expect_gg(BayesPharma::traceplot(test_model,
+    predictors_col_name = NULL,
+    half_max_label = NULL)))
+  expect_error(expect_gg(BayesPharma::traceplot(test_model,
+    half_max_label = NULL)))
+
+})
+
+
