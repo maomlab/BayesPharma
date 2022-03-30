317dd8a124f8ff622f5f1c3f0f4d20818154270d
MadelineMartin <martin.mjm105@gmail.com>
2022-03-30 17:16:10 -0400

    fixed typos

--- tests/testthat/test-density_distributions.R
+++ tests/testthat/test-density_distributions.R
@@ -3,7 +3,7 @@ library(BayesPharma)
 
 load(file = "../testdata/ggplot_test_model.rda")
 
-testthat::test_that("traceplot is a ggplot object",{
+testthat::test_that("density_distributions is a ggplot object",{
   expect_gg(BayesPharma::density_distributions(test_model))
   expect_gg(BayesPharma::density_distributions(test_model,
     predictors_col_name = "_Intercept",
@@ -19,7 +19,7 @@ testthat::test_that("traceplot is a ggplot object",{
 })
 
 
-testthat::test_that("traceplot throws error if predictors_col_name and
+testthat::test_that("density_distributions throws error if predictors_col_name and
                     half_max_label are not characters",{
   expect_error(expect_gg(BayesPharma::density_distributions(test_model,
     predictors_col_name = NULL)))
--- tests/testthat/test-prior_posterior_densities.R
+++ tests/testthat/test-prior_posterior_densities.R
@@ -3,7 +3,7 @@ library(BayesPharma)
 
 load(file = "../testdata/ggplot_test_model.rda")
 
-testthat::test_that("traceplot is a ggplot object",{
+testthat::test_that("prior_posterior_densities is a ggplot object",{
   expect_gg(BayesPharma::prior_posterior_densities(test_model))
   expect_gg(BayesPharma::prior_posterior_densities(test_model,
      predictors_col_name = "_Intercept",
@@ -17,7 +17,7 @@ testthat::test_that("traceplot is a ggplot object",{
 })
 
 
-testthat::test_that("traceplot throws error if predictors_col_name and
+testthat::test_that("prior_posterior_densities throws error if predictors_col_name and
                     half_max_label are not characters",{
   expect_error(expect_gg(BayesPharma::prior_posterior_densities(test_model,
     predictors_col_name = NULL)))
