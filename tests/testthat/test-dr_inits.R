435b66003a2c5ff1d170ab1f7692fbf3c50d219d
Matthew O'Meara <maom@umich.edu>
2022-08-20 22:25:12 -0400

    Add expect_gg function
    Borrowed from bayesplot, expect_gg tests an object is
    a 'ggplot2' object and then tries to build it silently.
    
    tidy up the formatting in the tests and fix a few broken tests

--- tests/testthat/test-dr_inits.R
+++ tests/testthat/test-dr_inits.R
@@ -5,16 +5,24 @@ testthat::test_that("dr_inits with default inits", {
   inits <- dr_inits()
 
   testthat::expect_true("list" %in% class(inits))
-  testthat::expect_equal(inits, rep(list(list(ec50 = -9, hill = -1, top = 100,
-                                              bottom = 0)), 4))
+  testthat::expect_equal(
+    inits,
+    rep(list(list(ec50 = -9, hill = -1, top = 100, bottom = 0)), 4))
 
 })
 
-testthat::test_that("dr_inits with custom inits", {
-  inits <- dr_inits(ec50 = -6, hill = 0.5, top = 50, bottom = 0, chains = 2)
+testthat::test_that(
+  "dr_inits with custom inits", {
+  inits <- dr_inits(
+    ec50 = -6,
+    hill = 0.5,
+    top = 50,
+    bottom = 0,
+    chains = 2)
 
   testthat::expect_true("list" %in% class(inits))
-  testthat::expect_equal(inits, rep(list(list(ec50 = -6, hill = 0.5, top = 50,
-                                              bottom = 0)), 2))
+  testthat::expect_equal(
+    inits,
+    rep(list(list(ec50 = -6, hill = 0.5, top = 50, bottom = 0)), 2))
 
 })
--- tests/testthat/test-traceplot.R
+++ tests/testthat/test-traceplot.R
@@ -4,30 +4,49 @@ library(BayesPharma)
 load(file = "../testdata/ggplot_test_model.rda")
 
 testthat::test_that("traceplot is a ggplot object", {
-  testthat::expect_s3_class(BayesPharma::traceplot(test_model),
-    "ggplot")
-  testthat::expect_s3_class(BayesPharma::traceplot(test_model,
-    predictors_col_name = "_Intercept",
-    half_max_label = "ic50",
-    title = "test_model traceplot"),
-    "ggplot")
-  testthat::expect_s3_class(BayesPharma::traceplot(test_model,
-    predictors_col_name = "_Intercept",
-    half_max_label = "ic50",
-    title = NULL),
-    "ggplot")
+  
+  expect_gg(
+    BayesPharma::traceplot(model = test_model))
+  
+  expect_gg(
+    BayesPharma::traceplot(
+      model = test_model,
+      predictors_col_name = "_Intercept",
+      half_max_label = "ic50",
+      title = "test_model traceplot"))
+  
+  expect_gg(
+    BayesPharma::traceplot(
+      model = test_model,
+      predictors_col_name = "_Intercept",
+      half_max_label = "ic50",
+      title = NULL))
 
 })
 
 
-testthat::test_that("traceplot throws error if predictors_col_name and
-                    half_max_label are not characters", {
-  testthat::expect_error(expect_gg(BayesPharma::traceplot(test_model,
-    predictors_col_name = NULL)))
-  testthat::expect_error(expect_gg(BayesPharma::traceplot(test_model,
-    predictors_col_name = NULL,
-    half_max_label = NULL)))
-  testthat::expect_error(expect_gg(BayesPharma::traceplot(test_model,
-    half_max_label = NULL)))
+testthat::test_that(
+  paste0(
+    "traceplot throws error if predictors_col_name and half_max_label are not ",
+    "characters"), {
+      
+  testthat::expect_error(
+    expect_gg(
+      BayesPharma::traceplot(
+        model = test_model,
+        predictors_col_name = NULL)))
+
+  testthat::expect_error(
+    expect_gg(
+      BayesPharma::traceplot(
+        model = test_model,
+        predictors_col_name = NULL,
+        half_max_label = NULL)))
+
+  testthat::expect_error(
+    expect_gg(
+      BayesPharma::traceplot(
+        model = test_model,
+        half_max_label = NULL)))
 
 })
