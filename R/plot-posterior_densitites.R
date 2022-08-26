18a220cd2b7f5d34965eeb7674b1da0416af18a6
Matthew O'Meara <maom@umich.edu>
2022-08-26 11:16:34 -0400

    Refactor plotting functions into separate files

---
+++ R/plot-posterior_densitites.R
@@ -0,0 +1,101 @@
+#' Create a plot of the posterior density distributions of modeled parameters
+#' from brmsfit model
+#'
+#' @description plots the brmsfit model density distribution of each parameter
+#'   and the mean, lower confidence interval, and upper confidence interval.
+#'
+#' @param model brmsfit model.
+#' @param predictors_col_name string expression for predictors column in the
+#'   input data.frame (default = "_Intercept"). Predictors are the perturbations
+#'   tested during the experiment (i.e. Drug, Temperature, etc.).
+#' @param half_max_label string of the label for the half maximal that fits the
+#'   type of experiment that was done (i.e. ec50, ic50, ed50, id50, ld50, etc.).
+#' @param l_ci numeric unit of the lower confidence interval (default = 0.025)
+#' @param u_ci numeric unit of the upper confidence interval (default = 0.975)
+#' @param title_label string of the plot title. (default = "Posterior Density
+#'   Plots with Mean and 95\% CI")
+#' @return ggplot2::ggplot object.
+#'
+#' @examples
+#'\dontrun{
+#'   posterior_densities(
+#'     model = my_dr_model,
+#'     predictors_col_name = "predictors",
+#'     half_max_label = "ic50",
+#'     l_ci = 0.025,
+#'     u_ci = 0.975,
+#'     title_label = "Posterior Density Plots with Mean and 95% CI")
+#'}
+#'@export
+
+posterior_densities <- function(
+    model,
+    predictors_col_name = "_Intercept",
+    half_max_label = "ec50",
+    l_ci = 0.025,
+    u_ci = 0.975,
+    title_label = "Posterior Density Plots w/ Mean & 95% CI") {
+  posterior <- dplyr::bind_rows(
+    model %>%
+      tidybayes::tidy_draws() %>%
+      tidybayes::gather_variables() %>%
+      dplyr::filter(!stringr::str_detect(.variable, "__$")) %>%
+      dplyr::filter(!stringr::str_detect(.variable, "sigma")) %>%
+      dplyr::filter(!stringr::str_detect(.variable, "lprior"))
+  ) %>%
+    dplyr::rename(variables = .variable) %>%
+    dplyr::mutate(
+      variables = variables %>%
+        stringr::str_extract("b_[a-zA-Z0-9]+.{1,100}") %>%
+        stringr::str_remove("b_") %>%
+        stringr::str_extract("[a-zA-Z0-9]+.{1,100}") %>%
+        stringr::str_remove(predictors_col_name) %>%
+        stringr::str_extract("[a-zA-Z0-9]+.{1,100}") %>%
+        stringr::str_replace("ec50", half_max_label))
+  
+  ggplot2::ggplot(data = posterior) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(legend.position = "bottom") +
+    ggplot2::geom_density(
+      mapping = ggplot2::aes(
+        x = .value),
+      fill = "cyan2",
+      color = "black",
+      alpha = .9) +
+    ggplot2::geom_vline(
+      ggplot2::aes(xintercept = mean),
+      basic_stats(
+        model = model,
+        predictors_col_name = predictors_col_name,
+        half_max_label = half_max_label,
+        l_ci = l_ci,
+        u_ci = u_ci),
+      color = "red") +
+    ggplot2::geom_rect(
+      ggplot2::aes(xmin = -Inf, xmax = l_ci, ymin = -Inf, ymax = Inf),
+      basic_stats(
+        model = model,
+        predictors_col_name = predictors_col_name,
+        half_max_label = half_max_label,
+        l_ci = l_ci,
+        u_ci = u_ci),
+      color = "gray",
+      alpha = 0.5) +
+    ggplot2::geom_rect(
+      ggplot2::aes(xmin = u_ci, xmax = Inf, ymin = -Inf, ymax = Inf),
+      basic_stats(
+        model = model,
+        predictors_col_name = predictors_col_name,
+        half_max_label = half_max_label,
+        l_ci = l_ci,
+        u_ci = u_ci),
+      color = "gray",
+      alpha = 0.5) +
+    ggplot2::ggtitle(label = paste0(title_label)) +
+    ggplot2::facet_wrap(
+      facets = dplyr::vars(variables),
+      scales = "free") +
+    ggplot2::scale_y_continuous("Density") +
+    ggplot2::scale_x_continuous("Parameter Value") +
+    ggplot2::scale_fill_discrete("Distribution")
+}
---
+++ R/summary-basic_stats.R
@@ -0,0 +1,56 @@
+#' Displays a data.frame of basic statistical information about the model
+#' results
+#'
+#' @description data.frame containing summary statistics of brmsfit model.
+#'   The summary statistics included are mean, median, standard deviation,
+#'   lower confidence interval, and upper confidence interval.
+#'
+#' @param model brmsfit model
+#' @param predictors_col_name string expression for predictors column in the
+#'   input data.frame (default = "_Intercept"). Predictors are the perturbations
+#'   tested during the experiment (i.e. Drug, Temperature, etc.).
+#' @param half_max_label string of the label for the half maximal that fits the
+#'   type of experiment that was done (i.e. ec50, ic50, ed50, id50, ld50, etc.).
+#' @param l_ci numeric unit of the lower confidence interval (default = 0.025)
+#' @param u_ci numeric unit of the upper confidence interval (default = 0.975)
+#' @return tibble::tibble object.
+#'
+#' @examples
+#'\dontrun{
+#'   basic_stats(
+#'     model = my_dr_model,
+#'     predictors_col_name = "predictors",
+#'     half_max_label = "ic50",
+#'     l_ci = 0.025,
+#'     u_ci = 0.975)
+#'}
+#' @export
+
+basic_stats <- function(
+    model,
+    predictors_col_name = "_Intercept",
+    half_max_label = "ec50",
+    l_ci = 0.025,
+    u_ci = 0.975) {
+  
+  ple_info <- brms::fixef(model, probs = c(l_ci, u_ci))
+  print(paste0("lower CI:", l_ci, "upper CI:", u_ci))
+  
+  model %>%
+    posterior::summarise_draws(
+      "mean", "sd", "median") %>%
+    dplyr::filter(!stringr::str_detect(variable, "__$")) %>%
+    dplyr::filter(!stringr::str_detect(variable, "sigma")) %>%
+    dplyr::filter(!stringr::str_detect(variable, "lprior")) %>%
+    dplyr::select(-variable) %>%
+    cbind(l_ci = c(ple_info[, 3]),
+          u_ci = c(ple_info[, 4])) %>%
+    tibble::rownames_to_column("variables") %>%
+    dplyr::mutate(
+      variables = stringr::str_extract(variables, "[a-zA-Z0-9]+.{1,100}") %>%
+        stringr::str_remove(predictors_col_name)) %>%
+    dplyr::mutate(variables = stringr::str_extract(variables,
+                                                   "[a-zA-Z0-9]+.{1,100}") %>%
+                    stringr::str_replace("ec50", half_max_label))
+  
+}
