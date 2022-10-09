a02cd9a2deaa8d0d700ea75465b1c324542b8345
Matthew O'Meara <maom@umich.edu>
2022-10-09 11:40:01 -0400

    update for tests

--- tests/testthat/test-sigmoid_model.R
+++ tests/testthat/test-sigmoid_model.R
@@ -1,38 +1,39 @@
 
-# testthat::test_that("Sigmoid model fit with ideal data", {
-#   sigmoid <- function(ec50, hill, top, bottom, log_dose) {
-#     # simple version that doesn't handle 0-doses
-#     bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill))
-#   }
-#   data <- data.frame(
-#     log_dose = seq(-7, -5, length.out = 30)) |>
-#     dplyr::mutate(
-#       response = log_dose |>
-#         sigmoid(ec50 = -6, hill = 1, top = 1, bottom = 0, log_dose = .) |>
-#         rnorm(sd = .2))
-#
-#   model <- data |>
-#     BayesPharma::sigmoid_model(
-#       formula = BayesPharma::sigmoid_formula(),
-#       priors = BayesPharma::sigmoid_priors())
-# })
-#
-# testthat::test_that("Sigmoid model fit with zero doses", {
-#   sigmoid <- function(ec50, hill, top, bottom, log_dose) {
-#     # simple version that doesn't handle 0-doses
-#     bottom + (top - bottom) / (1 + 10^((ec50 - log_dose)*hill))
-#   }
-#   data <- data.frame(
-#     log_dose = seq(-7, -5, length.out = 30)) |>
-#     dplyr::mutate(
-#       response = log_dose |>
-#         sigmoid(ec50 = -6, hill = 1, top = 1, bottom = 0, log_dose = .) |>
-#         rnorm(sd = 0.2)) |>
-#     dplyr::bind_rows(
-#       data.frame(log_dose = -Inf, response = 0))
-#
-#   model <- data |>
-#     BayesPharma::sigmoid_model(
-#       formula = BayesPharma::sigmoid_formula(),
-#       priors = BayesPharma::sigmoid_priors())
-#})
+testthat::test_that("Sigmoid model fit with ideal data", {
+  data <- data.frame(
+    log_dose = seq(-7, -5, length.out = 30)) |>
+    dplyr::mutate(
+      response = rnorm(
+        n = length(log_dose), 
+        mean = BayesPharma::sigmoid(
+          ec50 = -6,
+          hill = 1,
+          top = 1,
+          bottom = 0,
+          log_dose = log_dose),
+        sd = .2))
+
+  model <- data |>
+    BayesPharma::sigmoid_model(
+      iter = 2000)
+})
+
+testthat::test_that("Sigmoid model fit with zero doses", {
+  data <- data.frame(
+    log_dose = seq(-7, -5, length.out = 30)) |>
+    dplyr::mutate(
+      response = rnorm(
+        n = length(log_dose), 
+        mean = BayesPharma::sigmoid(
+          ec50 = -6,
+          hill = 1,
+          top = 1,
+          bottom = 0,
+          log_dose = log_dose),
+        sd = .2)) |>
+    dplyr::bind_rows(
+      data.frame(log_dose = -Inf, response = 0))
+
+  model <- data |>
+    BayesPharma::sigmoid_model()
+})
