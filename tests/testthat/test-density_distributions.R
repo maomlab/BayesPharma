85e52d246f5699c5925e7ba99047c1fa754967fd
Matthew O'Meara <maom@umich.edu>
2022-10-10 17:33:14 -0400

    rename density_distributions to density_distributions_plot

--- tests/testthat/test-density_distributions.R
+++ tests/testthat/test-density_distributions.R
@@ -3,36 +3,57 @@ library(BayesPharma)
 
 load(file = "../testdata/ggplot_test_model.rda")
 
-testthat::test_that("density_distributions is a ggplot object", {
-  testthat::expect_s3_class(BayesPharma::density_distributions(test_model),
-    "ggplot")
-  testthat::expect_s3_class(BayesPharma::density_distributions(test_model,
-    predictors_col_name = "_Intercept",
-    half_max_label = "ic50",
-    sample_type = "Prior",
-    title = "test_model dens distributions"),
-    "ggplot")
-  testthat::expect_s3_class(BayesPharma::density_distributions(test_model,
-    predictors_col_name = "_Intercept",
-    half_max_label = "ic50",
-    sample_type = "Posterior",
-    title = NULL),
-    "ggplot")
+testthat::test_that(
+  desc = "simple density_distributions is a ggplot object",
+  code = {
+  expect_gg(
+    BayesPharma::density_distributions_plot(test_model))
+  })
 
-})
+testthat::test_that(
+  desc = "custom density_distributions is a ggplot object",
+  code = {
+  expect_gg(
+    BayesPharma::density_distributions_plot(
+      test_model,
+      predictors_col_name = "_Intercept",
+      half_max_label = "ic50",
+      sample_type = "Prior",
+      title = "test_model dens distributions"))
+  })
+
+testthat::test_that(
+  desc = "alt custom density_distributions is a ggplot object",
+  code = {
+  expect_gg(
+    BayesPharma::density_distributions_plot(
+      test_model,
+      predictors_col_name = "_Intercept",
+      half_max_label = "ic50",
+      sample_type = "Posterior",
+      title = NULL))
+  })
 
 
-testthat::test_that("density_distributions throws error if predictors_col_name
-                     and half_max_label are not characters", {
-  testthat::expect_error(expect_gg(BayesPharma::density_distributions(
-    test_model,
-    predictors_col_name = NULL)))
-  testthat::expect_error(expect_gg(BayesPharma::density_distributions(
-    test_model,
-    predictors_col_name = NULL,
-    half_max_label = NULL)))
-  testthat::expect_error(expect_gg(BayesPharma::density_distributions(
-    test_model,
-    half_max_label = NULL)))
+testthat::test_that(
+  desc = paste0(
+    "density_distributions throws error if predictors_col_name is not
+    a character"),
+  code = {
+  testthat::expect_error(
+    expect_gg(
+      BayesPharma::density_distributions_plot(
+        test_model,
+        predictors_col_name = NULL)))
+  })
 
-})
+testthat::test_that(
+  desc = paste0(
+    "density_distributions throws error if half_max_label is not a character"),
+  code = {  
+  testthat::expect_error(
+    expect_gg(
+      BayesPharma::density_distributions_plot(
+      test_model,
+      half_max_label = NULL)))
+  })
