62a5124263681190b54b5c41e72786a74b217af3
Matthew O'Meara <maom@umich.edu>
2022-10-11 16:53:24 -0400

    fix sigmiod_prior -> sigmoid_agonist/antagonist_prior
    start to remove the plotting functions in favor of directly using bayesplot

--- tests/testthat/test-sigmoid_formula.R
+++ tests/testthat/test-sigmoid_formula.R
@@ -1,8 +1,8 @@
 library(BayesPharma)
 library(tidymodels)
 
-testthat::test_that("sigmoid_formula with a constant predictor", {
-  formula <- BayesPharma::sigmoid_formula()
+testthat::test_that("sigmoid_agoinst_formula with a constant predictor", {
+  formula <- BayesPharma::sigmoid_agonist_formula()
 
   testthat::expect_true("brmsformula" %in% class(formula))
   testthat::expect_equal(formula$resp, "response")
@@ -23,8 +23,8 @@ testthat::test_that("sigmoid_formula with a constant predictor", {
   testthat::expect_equal(rlang::f_rhs(formula$pforms[[4]]), 1L)
 })
 
-testthat::test_that("sigmoid_formula with a substance predictor", {
-  formula <- BayesPharma::sigmoid_formula(
+testthat::test_that("sigmoid_agoinst_formula with a substance predictor", {
+  formula <- BayesPharma::sigmoid_agonist_formula(
     predictors = substance)
 
   testthat::expect_true("brmsformula" %in% class(formula))
@@ -46,8 +46,8 @@ testthat::test_that("sigmoid_formula with a substance predictor", {
   testthat::expect_equal(rlang::f_rhs(formula$pforms[[4]]), quote(substance))
 })
 
-testthat::test_that("sigmoid_formula with a grouped predictor", {
-  formula <- BayesPharma::sigmoid_formula(
+testthat::test_that("sigmoid_agoinst_formula with a grouped predictor", {
+  formula <- BayesPharma::sigmoid_agonist_formula(
     predictors = substance + (1 | batch))
 
   testthat::expect_true("brmsformula" %in% class(formula))
@@ -60,6 +60,79 @@ testthat::test_that("sigmoid_formula with a grouped predictor", {
     quote(sigmoid(ec50, hill, top, bottom, log_dose)))
 
   testthat::expect_equal(rlang::f_lhs(formula$pforms[[1]]), quote(ec50))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[1]]),
+                         quote(substance + (1 | batch)))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[2]]), quote(hill))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[2]]),
+                         quote(substance + (1 | batch)))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[3]]), quote(top))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[3]]),
+                         quote(substance + (1 | batch)))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[4]]), quote(bottom))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[4]]),
+                         quote(substance + (1 | batch)))
+})
+
+##########
+testthat::test_that("sigmoid_antagonist_formula with a constant predictor", {
+  formula <- BayesPharma::sigmoid_antagonist_formula()
+  
+  testthat::expect_true("brmsformula" %in% class(formula))
+  testthat::expect_equal(formula$resp, "response")
+  testthat::expect_equal(
+    rlang::f_lhs(formula$formula),
+    quote(response))
+  testthat::expect_equal(
+    rlang::f_rhs(formula$formula),
+    quote(sigmoid(ic50, hill, top, bottom, log_dose)))
+  
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[1]]), quote(ic50))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[1]]), 1L)
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[2]]), quote(hill))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[2]]), 1L)
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[3]]), quote(top))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[3]]), 1L)
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[4]]), quote(bottom))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[4]]), 1L)
+})
+
+testthat::test_that("sigmoid_antagonist_formula with a substance predictor", {
+  formula <- BayesPharma::sigmoid_antagonist_formula(
+    predictors = substance)
+  
+  testthat::expect_true("brmsformula" %in% class(formula))
+  testthat::expect_equal(formula$resp, "response")
+  testthat::expect_equal(
+    rlang::f_lhs(formula$formula),
+    quote(response))
+  testthat::expect_equal(
+    rlang::f_rhs(formula$formula),
+    quote(sigmoid(ic50, hill, top, bottom, log_dose)))
+  
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[1]]), quote(ic50))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[1]]), quote(substance))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[2]]), quote(hill))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[2]]), quote(substance))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[3]]), quote(top))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[3]]), quote(substance))
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[4]]), quote(bottom))
+  testthat::expect_equal(rlang::f_rhs(formula$pforms[[4]]), quote(substance))
+})
+
+testthat::test_that("sigmoid_antagonist_formula with a grouped predictor", {
+  formula <- BayesPharma::sigmoid_antagonist_formula(
+    predictors = substance + (1 | batch))
+  
+  testthat::expect_true("brmsformula" %in% class(formula))
+  testthat::expect_equal(formula$resp, "response")
+  testthat::expect_equal(
+    rlang::f_lhs(formula$formula),
+    quote(response))
+  testthat::expect_equal(
+    rlang::f_rhs(formula$formula),
+    quote(sigmoid(ic50, hill, top, bottom, log_dose)))
+  
+  testthat::expect_equal(rlang::f_lhs(formula$pforms[[1]]), quote(ic50))
   testthat::expect_equal(rlang::f_rhs(formula$pforms[[1]]),
                          quote(substance + (1 | batch)))
   testthat::expect_equal(rlang::f_lhs(formula$pforms[[2]]), quote(hill))
--- tests/testthat/test-sigmoid_prior.R
+++ tests/testthat/test-sigmoid_prior.R
@@ -2,9 +2,8 @@ library(BayesPharma)
 library(tidymodels)
 
 
-testthat::test_that("sigmoid_prior with default prior", {
-  prior <- BayesPharma::sigmoid_prior()
-
+testthat::test_that("sigmoid_agoinst_prior with default prior", {
+  prior <- BayesPharma::sigmoid_agoinst_prior()
   testthat::expect_true("brmsprior" %in% class(prior))
 
   #"prior" "class" "coef" "group" "resp"  "dpar"  "nlpar" "lb" "ub" "source"
@@ -15,7 +14,7 @@ testthat::test_that("sigmoid_prior with default prior", {
 
   testthat::expect_true(all(
     as.list(prior[2, ]) ==
-    c("normal(-1, 1)", "b", "", "", "", "", "hill", NA, "0.01", "user"),
+    c("normal(1, 1)", "b", "", "", "", "", "hill", NA, "-0.01", "user"),
     na.rm = TRUE))
 
   testthat::expect_true(all(
@@ -30,10 +29,10 @@ testthat::test_that("sigmoid_prior with default prior", {
 })
 
 
-testthat::test_that("sigmoid_prior with constant prior", {
-  prior <- BayesPharma::sigmoid_prior(
+testthat::test_that("sigmoid_agoinst_prior with constant prior", {
+  prior <- BayesPharma::sigmoid_agoinst_prior(
     ec50 = -7,
-    hill = -1,
+    hill = 1.2,
     top = 100,
     bottom = 0)
 
@@ -47,7 +46,7 @@ testthat::test_that("sigmoid_prior with constant prior", {
 
   testthat::expect_true(all(
     as.list(prior[2, ]) ==
-      c("constant(-1)", "b", "", "", "", "", "hill", NA, NA, "user"),
+      c("constant(1.2)", "b", "", "", "", "", "hill", NA, NA, "user"),
     na.rm = TRUE))
 
   testthat::expect_true(all(
@@ -61,8 +60,8 @@ testthat::test_that("sigmoid_prior with constant prior", {
     na.rm = TRUE))
 })
 
-testthat::test_that("sigmoid_prior with custom prior", {
-  prior <- BayesPharma::sigmoid_prior(
+testthat::test_that("sigmoid_agoinst_prior with custom prior", {
+  prior <- BayesPharma::sigmoid_agoinst_prior(
     ec50 = brms::prior(student_t(3, -7, 3), nlpar = "ec50", ub = -4),
     hill = brms::prior_string(paste0("constant(-1)"), nlpar = "hill"),
     top = brms::prior(beta(10, 1), nlpar = "top"),
@@ -92,13 +91,92 @@ testthat::test_that("sigmoid_prior with custom prior", {
     na.rm = TRUE))
 })
 
-testthat::test_that("sigmoid_prior in agonist mode", {
-  prior <- BayesPharma::sigmoid_prior(inhibitor = FALSE)
+###
+testthat::test_that("sigmoid_antagonist_prior with default prior", {
+  prior <- BayesPharma::sigmoid_antagonist_prior()
   testthat::expect_true("brmsprior" %in% class(prior))
-
+  
+  #"prior" "class" "coef" "group" "resp"  "dpar"  "nlpar" "lb" "ub" "source"
+  testthat::expect_true(all(
+    as.list(prior[1, ]) ==
+      c("normal(-7, 2.5)", "b", "", "", "", "", "ic50", NA, NA, "user"),
+    na.rm = TRUE))
+  
   testthat::expect_true(all(
     as.list(prior[2, ]) ==
-      c("normal(1, 1)", "b", "", "", "", "", "hill", -0.01, NA, "user"),
+      c("normal(-1, 1)", "b", "", "", "", "", "hill", NA, "0.01", "user"),
     na.rm = TRUE))
+  
+  testthat::expect_true(all(
+    as.list(prior[3, ]) ==
+      c("normal(100, 25)", "b", "", "", "", "", "top", NA, NA, "user"),
+    na.rm = TRUE))
+  
+  testthat::expect_true(all(
+    as.list(prior[4, ]) ==
+      c("normal(0, 25)", "b", "", "", "", "", "bottom", NA, NA, "user"),
+    na.rm = TRUE))
+})
 
+
+testthat::test_that("sigmoid_antagonist_prior with constant prior", {
+  prior <- BayesPharma::sigmoid_antagonist_prior(
+    ic50 = -7,
+    hill = -1,
+    top = 100,
+    bottom = 0)
+  
+  testthat::expect_true("brmsprior" %in% class(prior))
+  
+  #"prior" "class" "coef" "group" "resp" "dpar" "nlpar" "lb" "ub" "source"
+  testthat::expect_true(all(
+    as.list(prior[1, ]) ==
+      c("constant(-7)", "b", "", "", "", "", "ic50", NA, NA, "user"),
+    na.rm = TRUE))
+  
+  testthat::expect_true(all(
+    as.list(prior[2, ]) ==
+      c("constant(-1)", "b", "", "", "", "", "hill", NA, NA, "user"),
+    na.rm = TRUE))
+  
+  testthat::expect_true(all(
+    as.list(prior[3, ]) ==
+      c("constant(100)", "b", "", "", "", "", "top", NA, NA, "user"),
+    na.rm = TRUE))
+  
+  testthat::expect_true(all(
+    as.list(prior[4, ]) ==
+      c("constant(0)", "b", "", "", "", "", "bottom", NA, NA, "user"),
+    na.rm = TRUE))
+})
+
+testthat::test_that("sigmoid_antagonist_prior with custom prior", {
+  prior <- BayesPharma::sigmoid_antagonist_prior(
+    ic50 = brms::prior(student_t(3, -7, 3), nlpar = "ic50", ub = -4),
+    hill = brms::prior_string("constant(-1)", nlpar = "hill"),
+    top = brms::prior(beta(10, 1), nlpar = "top"),
+    bottom = brms::prior(beta(1, 10), nlpar = "bottom"))
+  
+  testthat::expect_true("brmsprior" %in% class(prior))
+  
+  #"prior" "class" "coef" "group" "resp" "dpar" "nlpar" "lb" "ub" "source"
+  testthat::expect_true(all(
+    as.list(prior[1, ]) ==
+      c("student_t(3, -7, 3)", "b", "", "", "", "", "ec50", NA, -4, "user"),
+    na.rm = TRUE))
+  
+  testthat::expect_true(all(
+    as.list(prior[2, ]) ==
+      c("constant(-1)", "b", "", "", "", "", "hill", NA, NA, "user"),
+    na.rm = TRUE))
+  
+  testthat::expect_true(all(
+    as.list(prior[3, ]) ==
+      c("beta(10, 1)", "b", "", "", "", "", "top", NA, NA, "user"),
+    na.rm = TRUE))
+  
+  testthat::expect_true(all(
+    as.list(prior[4, ]) ==
+      c("beta(1, 10)", "b", "", "", "", "", "bottom", NA, NA, "user"),
+    na.rm = TRUE))
 })
