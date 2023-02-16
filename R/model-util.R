97afa6d71d4f99cb43cd5994281f2e4e2fc3992e
Matthew O'Meara <maom@Matthews-MacBook-Air.local>
2023-02-16 16:34:31 -0500

    fix ambiguity in the prepare_init function variable naming

--- R/model-util.R
+++ R/model-util.R
@@ -5,12 +5,14 @@
 #' @return function returning a numeric array of length 1.
 prepare_init <- function(init) {
   if (methods::is(init, "function")) {
+    # init is a function, check that it returns a numeric array of dimension 1
     x <- init()
     assertthat::assert_that(methods::is(x, "array"))
     assertthat::assert_that(dim(x) == 1)
     assertthat::assert_that(is.numeric(x))
+    init_fn <- init
   } else if (is.numeric(init)) {
-    init <- function() {
+    init_fn <- function() {
       as.array(init)
     }
   } else {
@@ -18,20 +20,21 @@ prepare_init <- function(init) {
       "Initialization should either be a function that returns an array of
       length one or numeric"))
   }
-  init
+  init_fn
 }
 
 #' Prepare a brmsprior
 #'
-#' What this gives above brms::prior(...) and friends is
+#' What this gives above \code{brms::prior(...)} and friends is
 #'   1) it allow just giving a numeric value rather than constant(<value>)
-#'   2) if it is already a brmsprior it checks that it has specified arguments
+#'   2) if it is already a \code{brms::brmsprior} it checks that it has
+#'      specified arguments
 #'
 #' This is used in models where want to allow user specified priors
 #' but make sure they are for the right parameters to make sure the
 #' model is well specified.
 #'
-#' @param prior \code{brmsprior} or numeric.
+#' @param prior \code{brms::brmsprior} or numeric.
 #' @param ... additional arguments to \code{brms::prior_string()}. If
 #'     \code{prior} is a \code{brmsprior} then this will check that
 #'     the slots have the given values.  If prior is numeric, then
