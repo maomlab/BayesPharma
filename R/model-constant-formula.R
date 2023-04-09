44326def4e001a9e50c1a12c86893f0ef7fa9364
Matthew O'Meara <maom@Matthews-MacBook-Air.local>
2023-04-09 09:27:50 -0400

    Update constant_formula for the formula refactoring

--- R/model-constant-formula.R
+++ R/model-constant-formula.R
@@ -1,15 +1,18 @@
 #' Formula for a curve with a constant response
-#'
+#' 
 #' @description set-up an equation for formula objects with a constant response.
 #'
+#' @param response_variable character variable representing the response to
+#'     treatment. (Default: 'response')
+#' @param response_units character the units of the response. (Default: NULL)
 #' @param predictors Additional formula objects to specify predictors
 #'     of non-linear parameters. i.e. what perturbations/experimental
 #'     differences should be modeled separately? (Default: 1) should a
 #'     random effect be taken into consideration? i.e. cell number,
 #'     plate number, etc.
-#' @param ... additional arugments to \code{brms::brmsformula}
+#' @param ... additional arguments to \code{brms::brmsformula}
 #'
-#' @returns brmsformula
+#' @returns bpformula brmsformula
 #'
 #' @examples
 #'\dontrun{
@@ -17,14 +20,16 @@
 #'}
 #' @export
 constant_formula <- function(
+    response_variable = "response",
+    response_units = NULL,
     predictors = 1,
     ...) {
 
   constant_eq <- rlang::new_formula(
-    lhs = quote(response),
+    lhs = rlang::ensym(response_variable),
     rhs = rlang::enexpr(predictors))
 
-  formula <- brms::brmsformula(constant_eq, ...)
-
-  return(formula)
+  model_formula <- brms::brmsformula(constant_eq, ...)
+  class(model_formula) <- c("bpformula", class(model_formula))
+  model_formula
 }
