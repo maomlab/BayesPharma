7e613c286febe99a44b12a83f41c2cdaeb347a86
Matthew O'Meara <maom@Matthews-MacBook-Air.local>
2023-04-12 08:21:20 -0400

    Update function documentation

--- R/model-MuSyC-run.R
+++ R/model-MuSyC-run.R
@@ -1,18 +1,63 @@
-#' MuSyC Synergy Model
+#' Fit a MuSyC Synergy Model
 #'
-#' Combined:
-#'      sample1 =
-#'
-#'
-#'      nlp_logE0 = X_logE0 * b_logE0
-#'                  _______   _______
-#'                     = 1    = param
-#'
-#'      nlp_logE1 = X_logE1 * b_logE0
-#'                  _______   ______
-#'                  =sample1
-#'
-#'
+#' @description The MuSyC synergy model is a bivariate functional form with
+#'   Bliss and Loewe synergy models models as special cases.
+#'   
+#'   The functional form is
+#'   \preformatted{
+#'     <response> ~ MuSyC(
+#'       <treatment 1> - <logd1scale>,
+#'       <treatment 2> - <logd2scale>,
+#'       logE0,
+#'       logC1, logE1, h1,
+#'       logC2, logE2, h2,
+#'       logE3, logalpha)}
+#'  See \code{\link{MuSyC}} for the full mathematical description of the
+#'  \code{MuSyC} function. By default the observed data (and therefore should be
+#'  columns in the input data data.frame) are
+#'  \itemize{
+#'    \item{\strong{<treatment 1>}: \code{logd1}, the \code{log10} of the dose
+#'      as a molar concentration of treatment 1}
+#'    \item{\strong{<treatment 2>}: \code{logd2}, the \code{log10} of the dose
+#'      as a molar concentration of treatment 2}
+#'    \item{\strong{<response>}: \code{response}, with unspecified units}
+#'  }
+#'  The \code{logd1scale} and \code{logd2scale} are used to center <treatment 1>
+#'  and <treatment 2> to make fitting more numerically stable. If they are not
+#'  in the input \code{data}, then they are taken to be the mean of
+#'  <treatment 1> and <treatment 2> respectively.
+#'      
+#'  The modeled parameters are
+#'  \itemize{
+#'    \item{\strong{logE0}: the \code{log(<response>)} when <treatment 1> = 0
+#'      and <treatment 2> = 0}
+#'    \item{\strong{logC1}: the \code{log(<treatment 1>} where when <treatment
+#'      2> = 0, the \code{<response>}} is halfway between \code{E0} and
+#'      \code{E1}
+#'    \item{\strong{logE1}: the \code{log(response)} when <treatment 1> =>
+#'      \code{Inf} and <treatment 2> = 0}
+#'    \item{\strong{h1}: the hill slope of the response with respect to
+#'      <treatment 1> when <treatment 1> = C1 and <treatment 2> = 0}. See
+#'      \code{\link{MuSyC_hi_to_si}} and \code{\link{MuSyC_si_hi}} for
+#'      converting between the slope (si) and hill slope (hi).
+#'    \item{\strong{logC2}: the dose of <treatment 2> where when <treatment 1> =
+#'      0, the \code{<response>}} is halfway between \code{E0} and \code{E2}
+#'    \item{\strong{logE2}: the \code{log(response)} when <treatment 2> =>
+#'      \code{Inf} and <treatment 1> = 0}
+#'    \item{\strong{h2}: the hill slope of the response with respect to
+#'      <treatment 2> when <treatment 2> = C2 and <treatment 1> = 0}. See
+#'      \code{\link{MuSyC_hi_to_si}} and \code{\link{MuSyC_si_hi}} for
+#'      converting between the slope (si) and hill slope (hi).
+#'    \item{\strong{logE3}: the \code{log(response)} when <treatment1 1> =>
+#'      \code{Inf} and <treatment 2> => \code{Inf}, modeling the synergistic
+#'      \emph{efficacy}}
+#'    \item{\strong{logaalpha}: the log of the synergistic \emph{potency} alpha.
+#'      When alpha > \code{1} the treatments are synergistic so that <treatment
+#'      1> shifts the response due to <treatment 2> to lower doses and visa
+#'      versa. When alpha < \code{1} the treatments are antagonistic so that
+#'      <treatment 1> shifts the response to <treatment 2> to higher doses and
+#'      visa versa}}
+#'  
 #' @param data \code{data.frame} of experimental data.  must contain columns
 #'     \code{response} and any predictors specified in the formula.
 #' @param formula \code{brmsformula} object. To create a dose-response
@@ -30,10 +75,15 @@
 #'     \code{list(adapt_delta = 0.99)}).
 #' @param stanvars \code{stanvars} code for the model. Default defines
 #'     the MuSyC function and genquant code.
-#' @param expose_functions boolean. Expose the BayesPharma functions for the
+#' @param expose_functions \code{logical}. Expose the BayesPharma functions for the
 #'   model [default: TRUE].
 #' @param ... additional arguments passed to \code{brms::brm}
 #'
+#' @references
+#' #' Wooten, D.J., Meyer, C.T., Lubbock, A.L.R. et al. MuSyC is a consensus
+#' framework that unifies multi-drug synergy metrics for combinatorial drug
+#' discovery. Nat Commun 12, 4607 (2021).
+#' https://doi.org/10.1038/s41467-021-24789-z
 #'
 #'@export
 MuSyC_model <- function(
