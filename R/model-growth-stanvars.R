c63d0ec2077ce2c597a94c976ca3e3e7cd9b8f4e
Matthew O'Meara <maom@Matthews-MacBook-Air.local>
2023-04-28 09:39:45 -0400

    fix a bunch of spelling mistakes

--- R/model-growth-stanvars.R
+++ R/model-growth-stanvars.R
@@ -2,10 +2,25 @@
 #'
 #' @note stanvar script of a sigmoid growth model response equation
 #'
+#' @examples
+#' \dontrun{
+#' brms::brm(
+#'   data = data.frame(
+#'     response = ...,
+#'     time = ...),
+#'   formula = brms::brmsformula(
+#'     response ~ growth_sigmoid(K, K0, rate, lambda, time),
+#'     K + K0 + rate + lambda ~ 1,
+#'     nl = TRUE),
+#'   prior = ...,
+#'   init =  ...,
+#'   stanvars = BayesPharma::growth_sigmoid_stanvar)
+#' }
+#' 
 #' @export
 growth_sigmoid_stanvar <- brms::stanvar(
   scode = paste(
-    "  real growth_richards(",
+    "  real growth_sigmoid(",
     "      real K,",
     "      real K0,",
     "      real rate,",
@@ -21,6 +36,21 @@ growth_sigmoid_stanvar <- brms::stanvar(
 #' Stan code for the Richards growth function.
 #'
 #' @note stanvar script of a Richard growth model response equation.
+#'
+#' @examples
+#' \dontrun{
+#' brms::brm(
+#'   data = data.frame(
+#'     response = ...,
+#'     time = ...),
+#'   formula = brms::brmsformula(
+#'     response ~ growth_richards(K, K0, rate, lambda, nu, time),
+#'     K + K0 + rate + lambda + nu ~ 1,
+#'     nl = TRUE),
+#'   prior = ...,
+#'   init =  ...,
+#'   stanvars = BayesPharma::growth_sigmoid_stanvar)
+#' }
 #'
 #' @export
 growth_richards_stanvar <- brms::stanvar(
--- R/model-sigmoid-stanvars.R
+++ R/model-sigmoid-stanvars.R
@@ -1,13 +1,12 @@
 #' Stan code for the sigmoid function.
 #'
-#' @note stanvar script of a sigmoid dose response equation.  For log
-#'     dose greater than negative infinity (dose greater than 0), the
-#'     sigmoid dose response equation is used. `ac50` is the half
-#'     maximal response, hill is the slope, top is the maximum
-#'     response, and bottom is the minimum response. For log dose
-#'     equal to negative infinity (dose equal to 0), if hill is
-#'     positive, then the bottom value is returned. If hill is
-#'     negative, then the top value is returned.
+#' @note stanvar script of a sigmoid dose response equation.  For log dose
+#'   greater than negative infinity (dose greater than 0), the sigmoid dose
+#'   response equation is used. `ac50` is the half maximal response, hill is the
+#'   slope, top is the maximum response, and bottom is the minimum response. For
+#'   log dose equal to negative infinity (dose equal to 0), if hill is positive,
+#'   then the bottom value is returned. If hill is negative, then the top value
+#'   is returned.
 #'
 #' @export
 sigmoid_stanvar <- brms::stanvar(
